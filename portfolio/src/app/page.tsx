0de3301895d1d2cf541f88c0569ef2d1577fb91e
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-07-31 19:54:46 -0400

    feat: remove README and package files, integrate react-simple-typewriter into Page component for enhanced user experience

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,3 +1,23 @@
+"use client";
+
+import { Typewriter } from "react-simple-typewriter";
+
 export default function Page() {
-  return <div></div>;
+  return (
+    <h1 style={{ fontSize: "2rem", color: "#fff" }}>
+      <Typewriter
+        words={[
+          "Bem-vindo ao meu site!",
+          "Desenvolvedor Full Stack.",
+          "Vamos criar algo incrível.",
+        ]}
+        loop={0}
+        cursor
+        cursorStyle="_"
+        typeSpeed={70}
+        deleteSpeed={50}
+        delaySpeed={1500}
+      />
+    </h1>
+  );
 }
