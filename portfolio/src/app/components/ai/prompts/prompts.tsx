1b69d85ca5bd40c548a3fc231314d95409bfbe48
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-08-04 18:17:05 -0400

    feat: restructure Prompts component to include buttons for navigation

--- portfolio/src/app/components/ai/prompts/prompts.tsx
+++ portfolio/src/app/components/ai/prompts/prompts.tsx
@@ -2,12 +2,14 @@ import styles from "./prompts.module.css";
 export function Prompts() {
   return (
     <div className={styles.prompts}>
-      <ul>
-        <li>About me</li>
-        <li>Projects</li>
-        <li>Contact</li>
-        <li>Skills</li>
-      </ul>
+      <div className={styles.container}>
+        <ul>
+          <li><button>About</button></li>
+          <li><button>Projects</button></li>
+          <li><button>Contact</button></li>
+          <li><button>Skills</button></li>
+        </ul>
+      </div>
     </div>
   );
 }
