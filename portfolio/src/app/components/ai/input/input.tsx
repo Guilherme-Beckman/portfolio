e3c6190e6b9c5c2d490f66452fd202db1a765ad6
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-08-05 15:36:46 -0400

    feat: add  button disable using 'use state'

--- portfolio/src/app/components/ai/input/input.tsx
+++ portfolio/src/app/components/ai/input/input.tsx
@@ -1,12 +1,21 @@
+'use client'
 import styles from "./input.module.css";
 import Image from "next/image";
 import arrow from "public/icons/arrow.svg";
+import { useState } from "react";
 export function AiInput() {
+  const [inputValue, setInputValue] = useState("");
+  const isButtonDisable = inputValue.trim() === "";
   return (
     <div className={styles.input}>
       <div className={styles.container}>
-        <input type="text" placeholder="Ask me something..." />
-        <button type="submit">
+        <input
+          type="text"
+          placeholder="Ask me something..."
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button type="submit" disabled={isButtonDisable}>
           <Image src={arrow} alt="Arrow" width={25} height={25} />
         </button>
       </div>
