8061face79c1bd667a5fd95db98fec940bf56a2b
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-08-04 21:57:40 -0400

    feat: refactor Page component to use FadeIn animations for enhanced visual appeal

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -2,17 +2,24 @@ import { TypingText } from "./components/typing-text/typing-text";
 import { AiInput } from "./components/ai/input/input";
 import { Prompts } from "./components/ai/prompts/prompts";
 import styles from "./page.module.css";
+import { FadeIn } from "./components/animations/fade-in";
 export default function Page() {
   return (
     <div className={styles.page}>
       <h1>
-        <span>AI Portfolio</span>
+        <FadeIn fromY={-20}>
+          <span className={styles.highlighted}>AI Portfolio</span>
+        </FadeIn>
         <br />
         <br />
-        <TypingText />
+        <FadeIn>
+          <TypingText />
+        </FadeIn>
       </h1>
-      <AiInput />
-      <Prompts />
+      <FadeIn>
+        <AiInput />
+        <Prompts />
+      </FadeIn>
     </div>
   );
 }
