3d424d2b21b9926ac64ac39fcf1079c1d68a08c8
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-08-06 18:02:53 -0400

    feat: add slideIn animation on bio in about-me page

--- portfolio/src/app/about-me/page.tsx
+++ portfolio/src/app/about-me/page.tsx
@@ -1,9 +1,12 @@
-import { Bio } from "../components/bio/bio"
-import styles from "./page.module.css"
-export default function AboutMe(){
-    return(
-        <div className={styles.aboutMe}>
-            <Bio/>
-        </div>
-    )
+import { Bio } from "../components/bio/bio";
+import styles from "./page.module.css";
+import { SlideIn } from "../components/animations/slide-in";
+export default function AboutMe() {
+  return (
+    <div className={styles.aboutMe}>
+      <SlideIn>
+        <Bio />
+      </SlideIn>
+    </div>
+  );
 }
