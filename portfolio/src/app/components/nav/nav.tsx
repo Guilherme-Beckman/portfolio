996d7cb18518348570701374be652c31fafa8ced
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-08-06 16:21:26 -0400

    feat: update nav to routing

--- portfolio/src/app/components/nav/nav.tsx
+++ portfolio/src/app/components/nav/nav.tsx
@@ -1,13 +1,33 @@
+"use client";
 import styles from "./nav.module.css";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 export function Nav() {
+  const pathName = usePathname();
   return (
     <div className={styles.nav}>
-        <ul>
-          <li>Home</li>
-          <li>About me</li>
-          <li>Projects</li>
-          <li>Blog</li>
-        </ul>
+      <ul>
+        <li className={pathName === "/" ? styles.active : ""}>
+          <Link className={styles.link} href={"/"}>
+            Home
+          </Link>
+        </li>
+        <li className={pathName === "/about-me" ? styles.active : ""}>
+          <Link className={styles.link} href={"/about-me"}>
+            About me
+          </Link>
+        </li>
+        <li className={pathName === "/projects" ? styles.active : ""}>
+          <Link className={styles.link} href={"/projetcs"}>
+            Projects
+          </Link>
+        </li>
+        <li className={pathName === "/blog" ? styles.active : ""}>
+          <Link className={styles.link} href={"/blog"}>
+            Blog
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 }
