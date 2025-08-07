f003ce6e772ccf2d4f110d301335d900a71bd7d1
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-08-07 15:03:08 -0400

    feat: add experiences to about me

--- portfolio/src/app/about-me/page.tsx
+++ portfolio/src/app/about-me/page.tsx
@@ -1,12 +1,18 @@
 import { Bio } from "../components/about-me/bio/bio";
 import styles from "./page.module.css";
 import { SlideIn } from "../components/animations/slide-in";
+import { Experiences } from "../components/about-me/experiences/experiences";
 export default function AboutMe() {
   return (
     <div className={styles.aboutMe}>
-      <SlideIn>
-        <Bio />
-      </SlideIn>
+      <div className={styles.bioExperiencesWrapper}>
+        <SlideIn>
+          <Bio />
+        </SlideIn>
+        <SlideIn fromX={50}>
+          <Experiences />
+        </SlideIn>
+      </div>
     </div>
   );
 }
