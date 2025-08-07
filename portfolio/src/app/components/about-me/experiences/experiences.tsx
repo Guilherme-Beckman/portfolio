b1109aafcdf51702fe7ab37c61ed45502ccc9c89
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-08-07 15:03:28 -0400

    test: adjust wrap

---
+++ portfolio/src/app/components/about-me/experiences/experiences.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import Profile from "public/images/pic.jpeg";
+import styles from "../bio/bio.module.css"
+
+export function Experiences() {
+  return (
+    <div className={styles.bio}>
+      <div className={styles.imageWrapper}>
+        <Image
+          src={Profile}
+          alt="Profile picture"
+          className={styles.mainImage}
+          priority
+        />
+      </div>
+      <div className={styles.info}>
+        <h2 className={styles.role}>FullStack Developer</h2>
+        <h1 className={styles.name}>Guilherme Beckman</h1>
+        <h3 className={styles.status}>
+          Currently working as an intern at IFMS
+        </h3>
+      </div>
+    </div>
+  );
+}
