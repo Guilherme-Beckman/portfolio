963847d122da5807c4ca330c9674eae1f922b38f
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-08-07 16:26:48 -0400

    feat: update files from Experience component

--- portfolio/src/app/components/about-me/experiences/experiences.tsx
+++ portfolio/src/app/components/about-me/experiences/experiences.tsx
@@ -1,25 +1,15 @@
-import Image from "next/image";
-import Profile from "public/images/pic.jpeg";
-import styles from "../bio/bio.module.css"
-
+import styles from "./experiences.module.css";
+import { WorkInfo } from "./work-info/work-info";
 export function Experiences() {
   return (
-    <div className={styles.bio}>
-      <div className={styles.imageWrapper}>
-        <Image
-          src={Profile}
-          alt="Profile picture"
-          className={styles.mainImage}
-          priority
-        />
-      </div>
-      <div className={styles.info}>
-        <h2 className={styles.role}>FullStack Developer</h2>
-        <h1 className={styles.name}>Guilherme Beckman</h1>
-        <h3 className={styles.status}>
-          Currently working as an intern at IFMS
-        </h3>
-      </div>
+    <div className={styles.experiences}>
+      <WorkInfo
+        companieName={"IFMS"}
+        model={"Presencial"}
+        period={"JAN 2025 - Present"}
+        local={"Corumbá - MS"}
+      />
+      
     </div>
   );
 }
---
+++ portfolio/src/app/components/about-me/experiences/timeline/timeline.tsx
@@ -0,0 +1,18 @@
+import styles from "./work-info.module.css";
+
+interface WorkInfoProps{
+  period: String;
+  companieName: String;
+  local: String;
+  model: String;
+}
+export function WorkInfo(workInfoProps: WorkInfoProps ) {
+  return (
+    <div className={styles.workInfo}>
+      <h2 className={styles.period}>{workInfoProps.period}</h2>
+      <h1 className={styles.companieName}>{workInfoProps.companieName}</h1>
+      <h2 className={styles.local}>{workInfoProps.local}</h2>
+      <h2 className={styles.model}>{workInfoProps.model}</h2>
+    </div>
+  );
+}
