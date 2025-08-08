c195d5eb564ef755cc0173f72932e886d4e2c51d
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-08-08 18:59:58 -0400

    feat: add const for experiences

--- portfolio/src/app/components/about-me/experiences/experiences.tsx
+++ portfolio/src/app/components/about-me/experiences/experiences.tsx
@@ -2,53 +2,9 @@ import styles from "./experiences.module.css";
 import { WorkInfo } from "./work-info/work-info";
 import Timeline from "./timeline/timeline";
 import { JobDescription } from "./job-description/job-description";
-import reactIcon from "public/icons/skills/react.svg"
-import typescriptIcon from "public/icons/skills/typescript.svg"
+import  experiencesData from "data/experienceData"
 export function Experiences() {
-  const experiencesData = [
-    {
-      companieName: "IFMS",
-      model: "Presencial",
-      period: "JAN 2025 - Present",
-      local: "Corumbá, MS, Brazil",
-      role: "FullStack / Infra",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut necessitatibus veritatis facilis tempore, magnam vel ullam dicta distinctio facere repellat iusto minus eius. Ullam facilis asperiores repellendus laborum dicta dolores!",
-      usedSkills: [
-        { name: "React", pathIcon: reactIcon },
-        { name: "TypeScript", pathIcon: typescriptIcon },
-      ],
-    },
-    {
-      companieName: "IFMS",
-      model: "Presencial",
-      period: "JAN 2025 - Present",
-      local: "Corumbá, MS, Brazil",
-
-      role: "FullStack / Infra",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut necessitatibus veritatis facilis tempore, magnam vel ullam dicta distinctio facere repellat iusto minus eius. Ullam facilis asperiores repellendus laborum dicta dolores!",
-      usedSkills: [
-        { name: "React", pathIcon: reactIcon },
-        { name: "TypeScript", pathIcon: typescriptIcon },
-        
-      ],
-    },
-    {
-      companieName: "IFMS",
-      model: "Presencial",
-      period: "JAN 2025 - Present",
-      local: "Corumbá, MS, Brazil",
-
-      role: "FullStack / Infra",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut necessitatibus veritatis facilis tempore, magnam vel ullam dicta distinctio facere repellat iusto minus eius. Ullam facilis asperiores repellendus laborum dicta dolores!",
-      usedSkills: [
-        { name: "React", pathIcon: reactIcon },
-        { name: "TypeScript", pathIcon: typescriptIcon },
-      ],
-    },
-  ];
+  
 
   return (
     <div className={styles.experiencesContainer}>
