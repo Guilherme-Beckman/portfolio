cd73c256d7a6253baf2841363c09e4c5e8439182
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-08-08 19:45:15 -0400

    feat: add icons for work info

--- portfolio/src/app/components/about-me/experiences/work-info/work-info.tsx
+++ portfolio/src/app/components/about-me/experiences/work-info/work-info.tsx
@@ -1,18 +1,26 @@
+import Image from "next/image";
 import styles from "./work-info.module.css";
-
-interface WorkInfoProps{
+import suitcaseIcon from "public/icons/suitcase-svgrepo-com.svg";
+import localIcon from "public/icons/location-pin-alt-1-svgrepo-com.svg";
+interface WorkInfoProps {
   period: string;
   companieName: string;
   local: string;
   model: string;
 }
-export function WorkInfo(workInfoProps: WorkInfoProps ) {
+export function WorkInfo(workInfoProps: WorkInfoProps) {
   return (
     <div className={styles.workInfo}>
       <h2 className={styles.period}>{workInfoProps.period}</h2>
       <h1 className={styles.companieName}>{workInfoProps.companieName}</h1>
-      <h2 className={styles.local}>{workInfoProps.local}</h2>
-      <h2 className={styles.model}>{workInfoProps.model}</h2>
+      <h2 className={styles.local}>
+        <Image src={localIcon} alt="local" />
+        {workInfoProps.local}
+      </h2>
+      <h2 className={styles.model}>
+        <Image src={suitcaseIcon} alt="suitecase"  />
+        {workInfoProps.model}
+      </h2>
     </div>
   );
 }
