f81c0af40094feb1d33257f4652c27eda881eedb
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-08-08 18:39:56 -0400

    feat: add svg icons to skills

--- portfolio/src/app/components/about-me/experiences/experiences.tsx
+++ portfolio/src/app/components/about-me/experiences/experiences.tsx
@@ -2,46 +2,49 @@ import styles from "./experiences.module.css";
 import { WorkInfo } from "./work-info/work-info";
 import Timeline from "./timeline/timeline";
 import { JobDescription } from "./job-description/job-description";
-
+import reactIcon from "public/icons/skills/react.svg"
+import typescriptIcon from "public/icons/skills/typescript.svg"
 export function Experiences() {
   const experiencesData = [
     {
       companieName: "IFMS",
       model: "Presencial",
       period: "JAN 2025 - Present",
-      local: "Corumbá - MS",
+      local: "Corumbá, MS, Brazil",
       role: "FullStack / Infra",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut necessitatibus veritatis facilis tempore, magnam vel ullam dicta distinctio facere repellat iusto minus eius. Ullam facilis asperiores repellendus laborum dicta dolores!",
       usedSkills: [
-        { name: "React", pathIcon: "/icons/react.svg" },
-        { name: "TypeScript", pathIcon: "/icons/typescript.svg" },
+        { name: "React", pathIcon: reactIcon },
+        { name: "TypeScript", pathIcon: typescriptIcon },
       ],
     },
     {
       companieName: "IFMS",
       model: "Presencial",
       period: "JAN 2025 - Present",
-      local: "Corumbá - MS",
+      local: "Corumbá, MS, Brazil",
+
       role: "FullStack / Infra",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut necessitatibus veritatis facilis tempore, magnam vel ullam dicta distinctio facere repellat iusto minus eius. Ullam facilis asperiores repellendus laborum dicta dolores!",
       usedSkills: [
-        { name: "React", pathIcon: "/icons/react.svg" },
-        { name: "TypeScript", pathIcon: "/icons/typescript.svg" },
+        { name: "React", pathIcon: "/icons/skills/skillsreact.svg" },
+        { name: "TypeScript", pathIcon: "/icons/skills/skills/typescript.svg" },
       ],
     },
     {
       companieName: "IFMS",
       model: "Presencial",
       period: "JAN 2025 - Present",
-      local: "Corumbá - MS",
+      local: "Corumbá, MS, Brazil",
+
       role: "FullStack / Infra",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut necessitatibus veritatis facilis tempore, magnam vel ullam dicta distinctio facere repellat iusto minus eius. Ullam facilis asperiores repellendus laborum dicta dolores!",
       usedSkills: [
-        { name: "React", pathIcon: "/icons/react.svg" },
-        { name: "TypeScript", pathIcon: "/icons/typescript.svg" },
+        { name: "React", pathIcon: "/icons/skills/skills/react.svg" },
+        { name: "TypeScript", pathIcon: "/icons/skills/skills/typescript.svg" },
       ],
     },
   ];
