b9285cebd9ae8699287d9ab61d9edeeb184377c9
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-08-27 15:29:53 -0400

    feat: add NavProject component and remove BackArrow component

--- portfolio/src/app/components/projects/more-about/more-about.tsx
+++ portfolio/src/app/components/projects/more-about/more-about.tsx
@@ -1,5 +1,9 @@
-import { BackArrow } from "../../ui/back-arrow";
+import { NavProject } from "./nav-project/nav-project";
 import styles from "./more-about.module.css";
 export function MoreAbout() {
-  return <div className={`${styles.moreAbout} animatedBg`}></div>;
+  return (
+    <div className={`${styles.moreAbout} animatedBg`}>
+      <NavProject />
+    </div>
+  );
 }
---
+++ portfolio/src/app/components/projects/more-about/nav-project/nav-project.tsx
@@ -0,0 +1,8 @@
+import { BackArrow } from "@/app/components/ui/back-arrow/back-arrow";
+export function NavProject() {
+  return (
+    <div>
+      <BackArrow />
+    </div>
+  );
+}
