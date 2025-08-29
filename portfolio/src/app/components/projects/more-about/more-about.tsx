3f4e1fc24f25ab167cba473cd0ce428b536a2e6b
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-08-28 20:58:04 -0400

    feat: enhance MoreAbout component to accept mainImageProps for dynamic image rendering

--- portfolio/src/app/components/projects/more-about/more-about.tsx
+++ portfolio/src/app/components/projects/more-about/more-about.tsx
@@ -1,15 +1,30 @@
 import { NavProject, NavProjectProps } from "./nav-project/nav-project";
 import styles from "./more-about.module.css";
+import Image from "next/image";
 interface MoreAboutProps {
   navProps?: NavProjectProps;
+  mainImageProps?: MainImageProjectProps;
 }
-export function MoreAbout({ navProps }: MoreAboutProps) {
+export interface MainImageProjectProps {
+  url?: string;
+  alt?: string;
+}
+export function MoreAbout({ navProps, mainImageProps }: MoreAboutProps) {
   return (
     <div className={`${styles.moreAbout} animatedBg`}>
       <NavProject
         gitHubUrl={navProps?.gitHubUrl}
         liveDemoUrl={navProps?.liveDemoUrl}
       />
+      <div className={styles.mainImageContainer}>
+        <Image
+          src={mainImageProps?.url || "/falback.png"}
+          alt={mainImageProps?.alt || "Project Image"}
+          width={400}
+          height={400}
+          className={styles.mainImage}
+        />
+      </div>
     </div>
   );
 }
--- portfolio/src/app/projects/project-example/page.tsx
+++ portfolio/src/app/projects/project-example/page.tsx
@@ -2,7 +2,10 @@ import { MoreAbout } from "@/app/components/projects/more-about/more-about";
 export default function ProjectExample() {
   return (
     <div>
-      <MoreAbout navProps={{gitHubUrl: "url", liveDemoUrl: "url"}}></MoreAbout>
+      <MoreAbout
+        navProps={{ gitHubUrl: "url", liveDemoUrl: "url" }}
+        mainImageProps={{url: "/icons/skills/angular-svgrepo-com.svg", alt:"Angular Project"}}
+      ></MoreAbout>
     </div>
   );
 }
