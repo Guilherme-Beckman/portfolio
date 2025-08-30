e6feef2351a0b18bd6bb47b831c8c4bb869cfe1e
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-08-30 13:31:07 -0400

    feat: add embla-carousel for enhanced video display in Demo component

--- portfolio/src/app/components/projects/more-about/demo/demo.tsx
+++ portfolio/src/app/components/projects/more-about/demo/demo.tsx
@@ -1,13 +1,28 @@
+"use client"
 import styles from "./demo.module.css";
 import Video from "next-video";
 import Instaplay from "player.style/instaplay/react";
+import useEmblaCarousel from "embla-carousel-react";
 export function Demo() {
+  const [emblaRef] = useEmblaCarousel();
   return (
     <div className={styles.demo}>
-      <Video
-        src={"https://cdn.pixabay.com/video/2025/08/20/298643_large.mp4"}
-        theme={Instaplay}
-      />
+      <div className={styles.embla} ref={emblaRef}>
+        <div className={styles.embla__container}>
+          <div className={styles.embla__slide}>
+            <Video
+              src={"https://cdn.pixabay.com/video/2025/08/20/298643_large.mp4"}
+              theme={Instaplay}
+            />
+          </div>
+          <div className={styles.embla__slide}>
+            <Video
+              src={"https://cdn.pixabay.com/video/2025/08/20/298643_large.mp4"}
+              theme={Instaplay}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
