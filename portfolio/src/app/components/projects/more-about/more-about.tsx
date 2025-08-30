7ff510cc03246aa4b4ff776499bd77795fc15d9d
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-08-30 18:52:07 -0400

    feat: update MoreAbout component to include ContactCard and refactor styles for better layout

--- portfolio/src/app/components/projects/more-about/more-about.tsx
+++ portfolio/src/app/components/projects/more-about/more-about.tsx
@@ -17,6 +17,7 @@ import {
   DevelopmentTimelineProps,
 } from "./development-timeline/development-timeline";
 import { Demo, DemoProps } from "./demo/demo";
+import ContactCard from "../../about-me/contacts/contacs";
 interface MoreAboutProps {
   navProps?: NavProjectProps;
   mainImageProps?: MainImageProjectProps;
@@ -48,51 +49,56 @@ export function MoreAbout({
   demo,
 }: MoreAboutProps) {
   return (
-    <div className={`${styles.moreAbout} animatedBg`}>
-      <NavProject
-        gitHubUrl={navProps?.gitHubUrl}
-        liveDemoUrl={navProps?.liveDemoUrl}
-      />
-      <div className={styles.mainImageContainer}>
-        <Image
-          src={mainImageProps?.url || "/falback.png"}
-          alt={mainImageProps?.alt || "Project Image"}
-          width={400}
-          height={400}
-          className={styles.mainImage}
+    <div className={styles.moreAboutContainer}>
+      <div className={`${styles.moreAbout} animatedBg`}>
+        <NavProject
+          gitHubUrl={navProps?.gitHubUrl}
+          liveDemoUrl={navProps?.liveDemoUrl}
         />
-      </div>
-      <h1 className={styles.title}>{title}</h1>
-      <h2 className={styles.subtitle}>{subtitle}</h2>
-      <div className={styles.badgesContainer}>
-        {badges.map((badge, index) => (
-          <Badge key={index} name={badge.name} color={badge.color} />
-        ))}
-      </div>
-      <div className={styles.demo}>
-        <Demo slides={demo.slides} />
-      </div>
-      <div className={styles.content}>
-        <div className={styles.mainContent}>
-          <div className={styles.projectOverviewContainer}>
-            <div className={styles.projectOverviewTitle}>
-              <Code color="#9ae5f3" />
-              <h3>Project Overview</h3>
+        <div className={styles.mainImageContainer}>
+          <Image
+            src={mainImageProps?.url || "/falback.png"}
+            alt={mainImageProps?.alt || "Project Image"}
+            width={400}
+            height={400}
+            className={styles.mainImage}
+          />
+        </div>
+        <h1 className={styles.title}>{title}</h1>
+        <h2 className={styles.subtitle}>{subtitle}</h2>
+        <div className={styles.badgesContainer}>
+          {badges.map((badge, index) => (
+            <Badge key={index} name={badge.name} color={badge.color} />
+          ))}
+        </div>
+        <div className={styles.demo}>
+          <Demo slides={demo.slides} />
+        </div>
+        <div className={styles.content}>
+          <div className={styles.mainContent}>
+            <div className={styles.projectOverviewContainer}>
+              <div className={styles.projectOverviewTitle}>
+                <Code color="#9ae5f3" />
+                <h3>Project Overview</h3>
+              </div>
+              <p className={styles.description}>{description}</p>
             </div>
-            <p className={styles.description}>{description}</p>
+            <div className={styles.keyFeaturesContainer}>
+              <KeyFeatures features={features} />
+            </div>
+            <div className={styles.technicalSpecificationsContainer}>
+              <TechnicalSpecifications {...technicalSpecifications} />
+            </div>
           </div>
-          <div className={styles.keyFeaturesContainer}>
-            <KeyFeatures features={features} />
-          </div>
-          <div className={styles.technicalSpecificationsContainer}>
-            <TechnicalSpecifications {...technicalSpecifications} />
-          </div>
-        </div>
 
-        <div className={styles.sideContent}>
-          <ProjectStatistics statistics={projectStatistics.statistics} />
-          <DevelopmentTimeline events={developmentTimeline.events} />
+          <div className={styles.sideContent}>
+            <ProjectStatistics statistics={projectStatistics.statistics} />
+            <DevelopmentTimeline events={developmentTimeline.events} />
+          </div>
         </div>
+      </div>
+      <div className={styles.contact}>
+        <ContactCard />
       </div>
     </div>
   );
