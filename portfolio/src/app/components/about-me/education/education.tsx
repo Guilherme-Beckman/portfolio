3058e1460f596c780dc7b104e3e005e0df1bde2a
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-09-04 11:53:37 -0400

    feat: add translations

--- portfolio/src/app/components/about-me/education/education.tsx
+++ portfolio/src/app/components/about-me/education/education.tsx
@@ -1,9 +1,10 @@
 import styles from "./education.module.css";
+import { useTranslations } from "next-intl";
 
 interface EducationProps {
-  cert: string;
-  course: string;
-  school: string;
+  certKey: string;
+  courseKey: string;
+  schoolKey: string;
   date: string;
 }
 
@@ -12,17 +13,19 @@ interface Props {
 }
 
 export function Education({ educationProps }: Props) {
+  const t = useTranslations("Education");
+
   return (
     <div className={`${styles.education} animatedBg`}>
-      <h2 className={styles.title}>EDUCATION</h2>
+      <h2 className={styles.title}>{t("educationTitle")}</h2>
 
       <div className={styles.section}>
         {educationProps.map((education, index) => (
           <div key={index} className={styles.educationItem}>
             <p className={styles.date}>{education.date}</p>
-            <p className={styles.cert}>{education.cert}</p>
-            <p className={styles.course}>{education.course}</p>
-            <p className={styles.school}>{education.school}</p>
+            <p className={styles.cert}>{t(education.certKey)}</p>
+            <p className={styles.course}>{t(education.courseKey)}</p>
+            <p className={styles.school}>{t(education.schoolKey)}</p>
           </div>
         ))}
       </div>
