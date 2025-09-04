26c8f14654839927db8772f74559363dd0056141
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-09-04 11:09:20 -0400

    feat: add content for bio

--- portfolio/src/app/components/about-me/bio/bio.tsx
+++ portfolio/src/app/components/about-me/bio/bio.tsx
@@ -1,22 +1,25 @@
 import Image from "next/image";
 import Profile from "public/images/pic.jpeg";
 import styles from "./bio.module.css";
+import { useTranslations } from "next-intl";
+
 export function Bio() {
+  const t = useTranslations("Bio");
+
   return (
     <div className={`${styles.bio} animatedBg`}>
       <div className={styles.imageWrapper}>
         <Image
           src={Profile}
-          alt="Profile picture"
-
+          alt={t("alt")}
           className={styles.mainImage}
           priority
         />
       </div>
       <div className={styles.info}>
-        <h2 className={styles.role}>FullStack Developer</h2>
-        <h1 className={styles.name}>Guilherme Beckman</h1>
-        <h3 className={styles.status}>Currently working as an intern at IFMS</h3>
+        <h2 className={styles.role}>{t("role")}</h2>
+        <h1 className={styles.name}>{t("name")}</h1>
+        <h3 className={styles.status}>{t("status")}</h3>
       </div>
     </div>
   );
