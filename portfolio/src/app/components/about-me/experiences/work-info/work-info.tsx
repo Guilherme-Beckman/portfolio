18ba442c0d36c327e795470308ee604d5eb44542
Guilherme Beckman <guilhermebeckman3@gmail.com>
2025-09-04 16:09:38 -0400

    feat: add translations

--- portfolio/src/app/components/about-me/experiences/work-info/work-info.tsx
+++ portfolio/src/app/components/about-me/experiences/work-info/work-info.tsx
@@ -2,24 +2,29 @@ import Image from "next/image";
 import styles from "./work-info.module.css";
 import suitcaseIcon from "public/icons/suitcase-svgrepo-com.svg";
 import localIcon from "public/icons/location-pin-alt-1-svgrepo-com.svg";
+import { useTranslations } from "next-intl";
+
 interface WorkInfoProps {
   period: string;
-  companieName: string;
+  companieNameKey: string;
   local: string;
-  model: string;
+  modelKey: string;
 }
-export function WorkInfo(workInfoProps: WorkInfoProps) {
+
+export function WorkInfo({ period, companieNameKey, local, modelKey }: WorkInfoProps) {
+  const t = useTranslations("Experience");
+
   return (
     <div className={styles.workInfo}>
-      <h2 className={styles.period}>{workInfoProps.period}</h2>
-      <h1 className={styles.companieName}>{workInfoProps.companieName}</h1>
+      <h2 className={styles.period}>{period}</h2>
+      <h1 className={styles.companieName}>{t(companieNameKey)}</h1>
       <h2 className={styles.local}>
         <Image src={localIcon} alt="local" />
-        {workInfoProps.local}
+        {local}
       </h2>
       <h2 className={styles.model}>
-        <Image src={suitcaseIcon} alt="suitecase"  />
-        {workInfoProps.model}
+        <Image src={suitcaseIcon} alt="suitcase" />
+        {t(modelKey)}
       </h2>
     </div>
   );
--- portfolio/src/app/data/experienceData.ts
+++ portfolio/src/app/data/experienceData.ts
@@ -1,47 +1,40 @@
-import reactIcon from "public/icons/skills/react.svg";
-import typescriptIcon from "public/icons/skills/typescript.svg";
-
- const experiencesData = [
+import kongIcon from "public/icons/skills/kong-icon-svgrepo-com.svg";
+import consulIcon from "public/icons/skills/consul-svgrepo-com.svg";
+import typeScriptIcon from "public/icons/skills/typescript.svg";
+import javaScriptIcon from "public/icons/skills/js-svgrepo-com.svg";
+import laravelIcon from "public/icons/skills/laravel-svgrepo-com.svg";
+import phpIcon from "public/icons/skills/php-svgrepo-com.svg";
+import microservicesIcon from "public/icons/skills/microservice-icon.svg";
+import dockerIcon from "public/icons/skills/docker-svgrepo-com.svg";
+import nginxIcon from "public/icons/skills/nginx-svgrepo-com.svg";
+import mysqlIcon from "public/icons/skills/mysql-svgrepo-com.svg";
+import htmlIcon from "public/icons/skills/html-5-svgrepo-com.svg";
+import cssIcon from "public/icons/skills/css-3-svgrepo-com.svg";
+import shellIcon from "public/icons/skills/shell-svgrepo-com.svg";
+const experiencesData = [
   {
-    companieName: "IFMS",
-    model: "Presencial",
+    companieNameKey: "ifms",
+    modelKey: "onsite",
     period: "JAN 2025 - Present",
     local: "Corumbá, MS, Brazil",
-    role: "FullStack / Infra",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut necessitatibus veritatis facilis tempore, magnam vel ullam dicta distinctio facere repellat iusto minus eius. Ullam facilis asperiores repellendus laborum dicta dolores!",
+    roleKey: "fullstackInfra",
+    descriptionKey: "ifmsDescription",
     usedSkills: [
-      { name: "React", pathIcon: reactIcon },
-      { name: "TypeScript", pathIcon: typescriptIcon },
-    ],
-  },
-  {
-    companieName: "IFMS",
-    model: "Presencial",
-    period: "JAN 2025 - Present",
-    local: "Corumbá, MS, Brazil",
-
-    role: "FullStack / Infra",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut necessitatibus veritatis facilis tempore, magnam vel ullam dicta distinctio facere repellat iusto minus eius. Ullam facilis asperiores repellendus laborum dicta dolores!",
-    usedSkills: [
-      { name: "React", pathIcon: reactIcon },
-      { name: "TypeScript", pathIcon: typescriptIcon },
-    ],
-  },
-  {
-    companieName: "IFMS",
-    model: "Presencial",
-    period: "JAN 2025 - Present",
-    local: "Corumbá, MS, Brazil",
-
-    role: "FullStack / Infra",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut necessitatibus veritatis facilis tempore, magnam vel ullam dicta distinctio facere repellat iusto minus eius. Ullam facilis asperiores repellendus laborum dicta dolores!",
-    usedSkills: [
-      { name: "React", pathIcon: reactIcon },
-      { name: "TypeScript", pathIcon: typescriptIcon },
+      { name: "Kong", pathIcon: kongIcon },
+      { name: "Consul", pathIcon: consulIcon },
+      { name: "Typescript", pathIcon: typeScriptIcon },
+      { name: "JavaScript", pathIcon: javaScriptIcon },
+      { name: "HTML", pathIcon: htmlIcon },
+      { name: "CSS", pathIcon: cssIcon },
+      { name: "Laravel", pathIcon: laravelIcon },
+      { name: "PHP", pathIcon: phpIcon },
+      { name: "Microservices", pathIcon: microservicesIcon },
+      { name: "Docker", pathIcon: dockerIcon },
+      { name: "Nginx", pathIcon: nginxIcon },
+      { name: "MySql", pathIcon: mysqlIcon },
+      { name: "Shell", pathIcon: shellIcon },
     ],
   },
 ];
+
 export default experiencesData;
